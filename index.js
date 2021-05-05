05213a3cdf9d45a08c92216a7a992f1e99a3c8c4
SSaxena2001 <56838945+SSaxena2001@users.noreply.github.com>
2021-05-05 15:20:49 +0530

    Added index.ejs and styles.css

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const ThingSpeakClient = require('thingspeakclient');
 const port = process.env.PORT || 4000; 
 
 const app = express();
+app.set('view engine', 'ejs');
 const client = new ThingSpeakClient();
 app.use(express.json());
+app.use(express.static('public'));
 
 const channelid=12397; //Public Channel is being used for now. 
 const fieldId=1;
@@ -27,14 +29,28 @@ client.getLastEntryInFieldFeed(12397,fieldId,(err,response)=>{
     if(!err){
         console.log(`Getting the data for ${fieldId}`);
         fieldData=response;
+        console.log(fieldData.field1);
     }
     else {
         console.error(err);
     }
 });
 
+
 app.get('/', (req, res)=>{
-    res.send(fieldData);
+    res.render('index',{
+        entrydata: "No Motion Detected",
+    });
+});
+
+app.get('/logs', (req, res)=>{
+    res.render('index',{
+        entrydata: fieldData.field1,
+    });
+});
+
+app.post('/',(req, res)=>{
+    res.redirect('/logs'); 
 });
 
 app.listen(port,()=>{
